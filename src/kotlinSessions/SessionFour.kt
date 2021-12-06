30ba8344ce099cddfc4e6e10a089a00d4246f177
Scrappers <scrappers.tm@gmail.com>
2021-12-05 23:16:05 -0600

    Refractory koltin docs

--- src/kotlinSessions/SessionFour.kt
+++ src/kotlinSessions/SessionFour.kt
@@ -11,7 +11,10 @@ package kotlinSessions
 // Functions that don't return any value and they are usually called Unit Functions or Methods.
 // Functions that have a return value, you can declare a return type using the colon ': Type' after the function names.
 
-// 1) Unit Functions :
+/**
+ * A Unit function used for making pizzas, packaging and delivering them.
+ * @return returns nothing.
+ */
 fun makePizza(): Unit{
     // a unit function is a process function that doesn't have a return value
     println("---------------------------My Pizza Task-----------------------------")
@@ -22,13 +25,19 @@ fun makePizza(): Unit{
     println("---------------------------My Pizza Task-----------------------------")
 }
 
-// 2) Return functions :
+/**
+ * A function used for adding two numbers and returning the result.
+ * @return returns out a sum of two numbers.
+ */
 fun add(): Int {
     // a function that adds 2 numbers and output the result
     return 2 + 3
 }
 
-// 3) String return function :
+/**
+ * A function used for printing out an id using [String]s.
+ * @return the id in a String format.
+ */
 fun myID(): String {
     return "Amer\n" +
             "29 yrs old \n" +
@@ -36,15 +45,26 @@ fun myID(): String {
             "ID : 2982319842093091 \n"
 }
 // 4) a return function can return any type, even arrays and ranges :
-// a range returning function
+/**
+ * A function used for returning an absolute [IntRange].
+ * @return a range with upper limit and lower limit.
+ */
 fun absoluteRange(): IntRange {
     return 2..20
 }
-// a progression returning function
+
+/**
+ * A function used for returning an absolute [IntProgression].
+ * @return a progression with an upper and a lower limit.
+ */
 fun absoluteProgression(): IntProgression {
     return 20 downTo 10
 }
-// an array returning function
+
+/**
+ * A function used for returning an [ArrayList] of names.
+ * @return a list of student names.
+ */
 fun studentNames(): ArrayList<String> {
     val names: ArrayList<String> = ArrayList()
     names.add("Micheal")
@@ -56,17 +76,33 @@ fun studentNames(): ArrayList<String> {
 // '()' are double parenthesis for passing parameters or variables locally to the function
 // so if we want to add x and y and return the sum then x and y can be function's parameter variables.
 // example
+
+/**
+ * Adds two numbers and returns the result.
+ * @param x first number.
+ * @param y second number.
+ * @return the result of the summation of x and y.
+ */
 fun getSum(x: Int, y:Int): Int {
     return x + y
 }
 // We can modify the previous examples to use a parameterized functions.
 // modify the studentNames function to accept a list parameter and wrap it inside a dynamic array, an ArrayList
+
+/**
+ * Wraps a [List] of defaults into an [ArrayList] and returns a new [ArrayList].
+ * @param defaults a default list to add to our new [ArrayList].
+ * @return an [ArrayList] that wraps a list of strings.
+ */
 fun studentNames(defaults: List<String>): ArrayList<String> {
     return ArrayList(defaults)
 }
 
 /**
  * The main function is the entry point for any kotlin application.
+ * @return nothing.
+ *
+ * @author pavl_g.
  */
 fun main(): Unit {
     // call the pizza task
